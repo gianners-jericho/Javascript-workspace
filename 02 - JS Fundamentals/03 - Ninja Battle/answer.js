47af05bc5ee272609aa09ddbc0f6014d2027d77e
Dreyyy25 <123479956+Dreyyy25@users.noreply.github.com>
2026-09-07 19:58:27 +0800

    feat: add Ninja Battle implementation and remove temp.md

---
+++ 02 - JS Fundamentals/03 - Ninja Battle/answer.js
@@ -0,0 +1,37 @@
+var ninja1 = {
+  hp: 100,
+  strength: 15,
+  attack: function() {
+     //your code
+     return Math.floor(Math.random() * (this.strength + 1));
+  }
+}
+var ninja2 = {
+  hp: 150,
+  strength: 10,
+  attack: function() {
+    //your code
+    return Math.floor(Math.random() * (this.strength + 1));
+  }
+}
+
+//ROUND's
+for (let i = 1; i <= 10; i++) {
+    console.log("===" + i + "===");
+
+    var damage1 = ninja1.attack();
+    ninja2.hp -= damage1;
+    console.log("Ninja1 attacks Ninja2 and does a damage of " + damage1 + "!   Ninja1 health: " + ninja1.hp + ".  Ninja2 health: " + ninja2.hp);
+
+    var damage2 = ninja2.attack();
+    ninja1.hp -= damage2;
+    console.log("Ninja2 attacks Ninja1 and does a damage of " + damage2 + "!   Ninja2 health: " + ninja2.hp + ".  Ninja1 health: " + ninja1.hp);
+}
+
+if(ninja1.hp > ninja2.hp) {
+    console.log("Ninja1 WINS!!!!!");
+}else if(ninja2.hp > ninja1.hp) {
+    console.log("Ninja2 WINS!!!!!");
+}else{
+    console.log("TIE");
+}
