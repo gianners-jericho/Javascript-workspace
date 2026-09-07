632be12f9dea135d4d20646b97c3b51dacca328a
CorgiusMinimus <wendellravago@gmail.com>
2026-09-07 18:23:52 +0800

    refactor: clean-up dquery

--- 02 - JS Fundamentals/05 - JS dQuery/Ravago_dQuery.js
+++ 02 - JS Fundamentals/05 - JS dQuery/Ravago_dQuery.js
@@ -1,34 +1,31 @@
 class lib {
-    constructor(target, document){
+    constructor(target){
         this.target = target
-        this.document = document
+    }
+
+    elements() {
+        return document.querySelectorAll(this.target)
     }
 
     hide(){
-        const elements = document.querySelectorAll(this.target)
-
-        for(const element of elements){
+        for(const element of this.elements()){
             element.style.display = "none"
         }
     }
 
     show(){
-        const elements = document.querySelectorAll(this.target)
-
-        for(const element of elements){
+        for(const element of this.elements()){
             element.style.display = "block"
         }
     }
 
     click(callback){
-        const elements = document.querySelectorAll(this.target)
-
-        for(const element of elements){
+        for(const element of this.elements()){
             element.addEventListener('click', callback)
         }
     }
 }
 
 function $query(target) {
-    return new lib(target, document);
+    return new lib(target);
 }
