b58e1728e84615128de7f7bfe545e780f61adbf1
Alfredo Venturina <alfredoventurina@gmail.com>
2026-09-08 22:14:10 +0800

    finished es5 bikes

--- 03 - JS OOP/01 - Bike/bike_es5_with_proto.js
+++ 03 - JS OOP/01 - Bike/bike_es5_with_proto.js
@@ -1,30 +1,33 @@
 //ES5 - no prototype
 
-function ES5_Bike(price, max_speed, miles){
+function Bike(price, max_speed, miles){
 
     //attributes
     this.price = price;
     this.max_speed = max_speed;
     this.miles = miles;
 
-    //methods
-    this.displayInfo = function(){
-        console.log(this.price, this.max_speed, this.miles)
-    }
-    this.drive = function(){
-        console.log("driving");
-        this.miles += 10;
-    }
-    this.reverse = function(){
-        console.log("reversing");
-        this.miles -= 5;
-    }
-
 }
 
-const car1 = new ES5_Bike(100, 10, 10);
-const car2 = new ES5_Bike(100, 10, 10);
-const car3 = new ES5_Bike(100, 10, 10);
+//METHODS - attached to prototype object
+Bike.prototype.displayInfo = function(){
+    console.log(this.price, this.max_speed, this.miles)
+}
+
+Bike.prototype.drive = function(){
+    console.log("driving");
+    this.miles += 10;
+}
+
+Bike.prototype.reverse = function(){
+    console.log("reversing");
+    this.miles -= 10;
+}
+
+//INSTANCES
+const car1 = new Bike(100, 10, 10);
+const car2 = new Bike(100, 10, 10);
+const car3 = new Bike(100, 10, 10);
 
 //first instance drives three times, reverse once, then display info
 for(let i=0; i<3; i++) car1.drive();
