bd9f70084e1d96347846183fe81685145183996b
Alfredo Venturina <alfredoventurina@gmail.com>
2026-09-10 02:34:52 +0800

    finished 9 ways of printing

---
+++ 04 - JS Advanced/01 - 9 Ways of Printing/print.js
@@ -0,0 +1,75 @@
+
+//functional
+function print_str(str) {
+    console.log(str);
+}
+print_str("1. I love Javascript!!!");
+
+
+//object method
+const printerObj = {
+    print: function(str) {
+        console.log(str);
+    }
+};
+printerObj.print("2. I love Javascript!!!");
+
+
+//constructor function instance method
+function ConstructorPrinter() {
+    this.print = function(str) {
+        console.log(str);
+    };
+}
+const constructorInstance = new ConstructorPrinter();
+constructorInstance.print("3. I love Javascript!!!");
+
+
+//prototype method
+function ProtoPrinter() {}
+ProtoPrinter.prototype.print = function(str) {
+    console.log(str);
+};
+const protoInstance = new ProtoPrinter();
+protoInstance.print("4. I love Javascript!!!");
+
+
+//ES6 classes and instance print method
+class ClassPrinter {
+    print(str) {
+        console.log(str);
+    }
+}
+const classInstance = new ClassPrinter();
+classInstance.print("5. I love Javascript!!!");
+
+
+//callbacks
+function executePrint(str, callback) {
+    callback(str);
+}
+executePrint("6. I love Javascript!!!", function(message) {
+    console.log(message);
+});
+
+
+//higher order function
+function createPrinter(prefix) {
+    return function(str) {
+        console.log(`${prefix} ${str}`);
+    };
+}
+const customPrinter = createPrinter("7.");
+customPrinter("I love Javascript!!!");
+
+
+//immediate functions
+(function(str) {
+    console.log(str);
+})("8. I love Javascript!!!");
+
+
+//document onclick event handler
+document.onclick = function() {
+    console.log("9. I love Javascript!!!");
+};
