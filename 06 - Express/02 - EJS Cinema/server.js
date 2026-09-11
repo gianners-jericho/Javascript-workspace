ff7750067a19a4859e84bb5cb0b0b2049a059776
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-11 14:31:36 +0800

    added my code for the cinema ejs activity

---
+++ 06 - Express/02 - EJS Cinema/server.js
@@ -0,0 +1,28 @@
+const express = require ("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+PORT = 8000;
+
+app.get("/", function(request, response) {
+    response.render("index");
+});
+
+app.get("/movies", function(request, response) {
+    response.render("movies")
+});
+
+app.get("/theaters", function(request, response) {
+    response.render("theaters")
+});
+
+app.get("/movies/new", function(request, response) {
+    response.render("forms")
+});
+
+app.listen(PORT, function(){
+    console.log(`Server is running on port ${PORT} and live at http://localhost:${PORT}`);
+});
