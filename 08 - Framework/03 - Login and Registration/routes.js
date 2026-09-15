95eaa3d1f203bc3e49beff025d546b83915016eb
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 21:33:43 +0800

    feat: added routes for logging in and registering students

--- 08 - Framework/03 - Login and Registration/routes.js
+++ 08 - Framework/03 - Login and Registration/routes.js
@@ -5,5 +5,7 @@ const StudentsController = require("./controllers/students");
 
 Router.get("/", StudentsController.index);
 Router.get("/students/profile", StudentsController.profile);
+Router.get("/students/login", StudentsController.login);
+Router.get("/students/register", StudentsController.regiser);
 
 module.exports = Router;
