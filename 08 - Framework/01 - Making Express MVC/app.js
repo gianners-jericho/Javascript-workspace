a4997529bd448ef17bc3eaf03d4ed4b0766d2811
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 14:23:49 +0800

    feat: added code for the routes and made a connection between routes.js and app.js

--- 08 - Framework/01 - Making Express MVC/app.js
+++ 08 - Framework/01 - Making Express MVC/app.js
@@ -6,6 +6,10 @@ app.set("view engine", "ejs");
 
 const PORT = 8000;
 
+const routes = require("./routes");
+
+app.use("/", routes);
+
 app.listen(PORT, function() {
     console.log(`Server is running on port ${PORT} and live at http://localhost:${PORT}`);
 });
