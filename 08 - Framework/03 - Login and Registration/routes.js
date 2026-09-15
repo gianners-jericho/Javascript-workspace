534e63f0ad9c8cd8069b5f960324f1cd096459b1
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 20:25:00 +0800

    refactor: cleaned up code for routesby removing unnecessary code

--- 08 - Framework/03 - Login and Registration/routes.js
+++ 08 - Framework/03 - Login and Registration/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const Router = express.Router();
 
-const StudentsController = require("./controllers/students memememememememememememememememememememememememememememe'smemememememememememememe'memememememememememememe'mememe'memememememememememememememememememememememememememememememememememememememememe'me'memememememememememememememememememememememememe'me'me'memememememememememememememememememememememememememememememememememememememememememememememememememememememememememe'me'me'me'me'me'me'me'me'mememememememememememememememememememememememememememememememememememememememememememe'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me''me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me'me''''me''''''me'me''me'me'me's'me'me'me's");
+const StudentsController = require("./controllers/students");
 
 Router.get("/", StudentsController.index);
 Router.get("/students/profile", StudentsController.profile);
