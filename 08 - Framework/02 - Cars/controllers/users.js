fe68d5a8091d1d7d9f651b68a3bfe51dc7a6757e
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 17:27:04 +0800

    feat: added code to handle getting all the car data once the index page loads

--- 08 - Framework/02 - Cars/controllers/users.js
+++ 08 - Framework/02 - Cars/controllers/users.js
@@ -1,9 +1,32 @@
+// Imports the users class
+const UserModel = require("../models/users");
+
 class UserController {
 
     static index(req, res) {
-        res.render("index");
+
+        // Instantiates the UserModel class
+        const userModel = new UserModel();
+        
+        // Calls the getCars method from the UserModel class
+        userModel.getCars(function(error, cars) {
+
+            // If something went wrong, return an error
+            if (error) {
+                console.log(error);
+                res.status(500).send("Database error");
+                return;
+            }
+
+            // For checking pull results
+            console.log(cars);
+            // If all is well, then return the result
+            res.render("index", {
+                cars: cars
+            });
+        });
     }
 
 }
 
-module.exports = UserController
+module.exports = UserController;
