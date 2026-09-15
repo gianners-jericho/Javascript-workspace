e7f212354394f8e5ced25e22612cd614a89f1c83
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 15:09:30 +0800

    feat: added a basic users model for the models folder

---
+++ 08 - Framework/01 - Making Express MVC/models/users.js
@@ -0,0 +1,26 @@
+class UserModel {
+    getUsers() {
+        return [
+            {
+                id: 1,
+                name: "Gian"
+            },
+            {
+                id: 2,
+                name: "Wendell"
+            },
+            {
+                id: 3,
+                name: "4th"
+            },
+            {
+                id: 4,
+                name: "Andrey"
+            },
+            {
+                id: 5,
+                name: "Sean"
+            }
+        ];
+    }
+}
