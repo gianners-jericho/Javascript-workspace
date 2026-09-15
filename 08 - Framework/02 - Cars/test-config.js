96c6882e899dd208385592f1bfb42b38e42bf1db
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 16:26:04 +0800

    feat: added a test file for my config.js with dotenv

---
+++ 08 - Framework/02 - Cars/test-config.js
@@ -0,0 +1,6 @@
+const config = require("./config");
+
+console.log("Host:", config.host);
+console.log("User:", config.user);
+console.log("Password:", config.password);
+console.log("Database:", config.database);
