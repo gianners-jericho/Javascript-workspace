111ff5a015896d1c739ff408f8b7cf8713ca16a6
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 18:07:10 +0800

    funct: added a function to count the number of cars

--- 08 - Framework/02 - Cars/models/users.js
+++ 08 - Framework/02 - Cars/models/users.js
@@ -2,8 +2,22 @@ const Model = require("./Model");
 
 class UserModel extends Model {
 
+    // Function for returning the count for the cars
+    countCars(callback) {
+        const sql = "SELECT COUNT (*) AS count FROM cars";
+
+        this.query(sql, function(error, results) {
+            if (error) {
+                callback(error, null);
+                return;
+            }
+            // Gives back the first result
+            callback(null, results[0]);
+        });
+    }
+
     // Function for getting all the cars data in the table
-    getCars(callback){
+    getCars(callback) {
         const sql = "SELECT * FROM cars";
 
         this.query(sql, function(error, results) {
