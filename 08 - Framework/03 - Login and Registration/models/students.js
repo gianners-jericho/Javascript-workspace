e00f7c337115a8c511c1f8e076feb2326ab907f8
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 21:31:13 +0800

    feat: added a new function that adds a new student

--- 08 - Framework/03 - Login and Registration/models/students.js
+++ 08 - Framework/03 - Login and Registration/models/students.js
@@ -20,6 +20,25 @@ class StudentsModel extends Model {
             callback(null, results[0]);
         });
     }
+
+    // For adding new students
+    registerStudent(firstName, lastName, email, password, callback) {
+        const sql =`
+            INSERT INTO students
+            (first_name, last_name, email, password)
+            VALUES (?, ?, ?, ?)
+        `;
+
+        this.query(sql, [firstName, lastName, email, password], function(error, results) {
+             
+            if (error) {
+                callback(error, null);
+                return;
+            }
+
+            callback(null, results);
+        }); 
+    }
 }
 
 module.exports = StudentsModel;
