0ff135b5646a1fd76ff6198af3bf881132915997
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-15 20:19:03 +0800

    feat: added sessions and for parsing url-encoded form data

--- 08 - Framework/03 - Login and Registration/app.js
+++ 08 - Framework/03 - Login and Registration/app.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: "students-secret",
+    resave: false, 
+    saveUninitalized: false
+}));
+
 const PORT = 8888;
 
 const routes = require("./routes");
