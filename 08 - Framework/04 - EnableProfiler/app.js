f80e4f810425956d4f7b58987ad0a370c0644aaf
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-16 15:58:42 +0800

    refactor: restructured the code

--- 08 - Framework/04 - EnableProfiler/app.js
+++ 08 - Framework/04 - EnableProfiler/app.js
@@ -2,19 +2,25 @@ const express = require("express");
 const session = require("express-session");
 
 const app = express();
+const PORT = 8888;
+const routes = require("./routes");
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
+app.use(
+  session({
     secret: "students-secret",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  }),
+);
 
-const PORT = 8888;
+app.use(routes);
 
-app.listen(PORT, function() {
-    console.log(`Server is running on port ${PORT} and live at http://localhost:${PORT}`);
+app.listen(PORT, function () {
+  console.log(
+    `Server is running on port ${PORT} and live at http://localhost:${PORT}`,
+  );
 });
