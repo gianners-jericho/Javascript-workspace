55e05196a286e01c76b5a430195cf1b5ccfb37c1
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-16 14:11:41 +0800

    refactor: adjusted the Model file to make it simpler and more adherent to async and await

--- 08 - Framework/04 - EnableProfiler/models/Model.js
+++ 08 - Framework/04 - EnableProfiler/models/Model.js
@@ -6,23 +6,31 @@ class Model {
     constructor() {
         // For auto connection to the database
         this.connection = mysql.createConnection(config);
+        
+        // Calls the checkConnection async function to run the log checks 
+        this.checkConnection();
 
-        // Check logs for database connection 
-        this.connection.then(function(connection) {
+    }
+    
+    // Check logs for database connection 
+    async checkConnection() {
+        try{
+            await this.connection;
             console.log(`Successfully connected to the ${process.env.DB_NAME} database!`);
-        }).catch(function(error) {
+        } catch (error) {
             console.log(`Database connection failed: ${error}`);
-        });
+        }
     }
 
-    // Async version for query handling function 
+    // Async version for query handling function, adjusted so it no longer does callbacks 
     async query(sql, values) {
-        const [results] = await this.connection.then(function(connection) {
-            return connection.execute(sql, values);
-        });
+        const connection = await this.connection;
+
+        const [results] = await connection.execute(sql, values);
 
         return results;
     }
 }
 
+let test = new Model();
 module.exports = Model;
