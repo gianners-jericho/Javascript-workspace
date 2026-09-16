e594d083b4f9b8a3ba22c760a1e29ef274826e83
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-16 20:02:19 +0800

    feat: added generic model

---
+++ 08 - Framework/05 - Ajax Sports Players/models/Model.js
@@ -0,0 +1,35 @@
+const mysql = require("mysql2/promise");
+const config = require("../config");
+
+class Model {
+
+    constructor() {
+        // For auto connection to the database
+        this.connection = mysql.createConnection(config);
+        
+        // Calls the checkConnection async function to run the log checks 
+        this.checkConnection();
+
+    }
+    
+    // Check logs for database connection 
+    async checkConnection() {
+        try{
+            await this.connection;
+            console.log(`Successfully connected to the ${process.env.DB_NAME} database!`);
+        } catch (error) {
+            console.log(`Database connection failed: ${error}`);
+        }
+    }
+
+    // Async version for query handling function, adjusted so it no longer does callbacks 
+    async query(sql, values) {
+        const connection = await this.connection;
+
+        const [results] = await connection.execute(sql, values);
+
+        return results;
+    }
+}
+
+module.exports = Model;
